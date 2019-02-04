89790424a7bb9d2456b1e59157bed416d6042ee3
alexharper84 <alexharper84@gmail.com>
2019-02-04 04:56:30 -0800

    jsx syntax sweetening

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 // import the react and reactDom libraries
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // create a react component
 
 const App = () => {
-  return <div>
-  <label className="label" for="name">Enter name:</label>
-  <input id="name" type="text"/>
-  <button style={{ backgroundColor: 'blue', color: 'red' }}>Submit</button>
-</div>
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter Name:";
+
+  return (
+    <div>
+      <label className="label" for="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "red" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
-
 // take the react component and shot it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
